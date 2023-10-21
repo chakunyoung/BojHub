b5cc3a2046126d97227cc5b95dcba1b385c21194
cha kun young <32626531+chakunyoung@users.noreply.github.com>
2023-10-21 20:11:43 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 580 ms, Memory: 47704 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+import java.util.function.*;
+
+class Edge implements Comparable<Edge> {
+    int u, v, w;
+    Edge(int u, int v, int w) {
+        this.u = u;
+        this.v = v;
+        this.w = w;
+    }
+
+    public int compareTo(Edge e) {
+        return this.w - e.w;
+    }
+}
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int[] parent;
+    
+    public static int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
+    }
+
+    public static void union(int u, int v) {
+        u = find(u);
+        v = find(v);
+        if (u != v) parent[u] = v;
+    }
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int eCount = Integer.parseInt(st.nextToken());
+        int vCount = Integer.parseInt(st.nextToken());
+        
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        parent = new int[eCount + 1];
+        
+        for (int i = 1; i <= eCount; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 0; i < vCount; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            int w = Integer.parseInt(st.nextToken());
+            pq.offer(new Edge(u, v, w));
+        }
+
+        int mstWeight = 0;
+
+        while (!pq.isEmpty()) {
+            Edge edge = pq.poll();
+            if (find(edge.u) != find(edge.v)) {
+                union(edge.u, edge.v);
+                mstWeight += edge.w;
+            }
+        }
+
+        System.out.println(mstWeight);
+    }
+}
