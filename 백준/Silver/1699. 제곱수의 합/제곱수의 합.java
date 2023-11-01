1b891208b3ded7a82fa74be07f956f0a549e8315
cha kun young <32626531+chakunyoung@users.noreply.github.com>
2023-11-01 12:13:22 +0900

    [Silver II] Title: 제곱수의 합, Time: 160 ms, Memory: 14928 KB -BaekjoonHub

--- 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
+++ 백준/Silver/1699. 제곱수의 합/제곱수의 합.java
@@ -1,51 +1,25 @@
-import java.io.*;
-import java.math.BigInteger; // subtract , multiply, add, mod
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-    static StringTokenizer st;
-    static StringBuilder sb = new StringBuilder();
 
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
 
+		int N = Integer.parseInt(br.readLine());
+		int[] dp = new int[N + 1];
 
+		for (int i = 1; i <= N; i++) {
+			dp[i] = i;
+			for (int j = 1; j * j <= i; j++) {
+				if (dp[i] > dp[i - j * j] + 1) {
+					dp[i] = dp[i - j * j] + 1; // 구해놓은 값으로 i를 계속 최소 개수로 맞춘다
+				}
+			}
+		}
 
-    public static void main(String[] args) throws IOException {
-        //StringTokenizer st = new StringTokenizer(br.readLine());
-        int number = Integer.parseInt(br.readLine());
-
-        int[] dp = new int[100001];
-        Arrays.fill(dp, 1000_000);
-
-        dp[0] = 0;
-        // dp 초기화
-        for(int i = 1 ; i<dp.length; i++){
-            if(i * i > 100000)
-                break;
-            int powNumber = i * i;
-            dp[powNumber] = 1;
-        }
-
-        // 1부터 100_000 까지 min을 채워나가기
-        for(int i = 1 ; i<dp.length; i++){
-            for(int j = 325; j >= 1; j--){
-                if(j * j > i) // 기준숫자보다 제곱이 커지면 의미가 없으니 넘김
-                    continue;
-                int jNumber = (int)Math.pow(j, 2);
-                int divide = i / jNumber;
-                int rest = i % jNumber;
-                if(i == 5 && j * jNumber == 4){
-                    System.out.println(divide + " " +rest);
-                }
-
-
-                dp[i] = Math.min(dp[i], dp[rest] + divide);
-            }
-        }
-        //System.out.println(Arrays.toString(dp));
-        System.out.println(dp[number]);
-
-
-    }
+		System.out.println(dp[N]);
+	}
 }
