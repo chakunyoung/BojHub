1a9570e64276a6feec6e90fe5b273825ee095a78
cha kun young <chakunyou@naver.com>
2023-12-08 20:20:54 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 496 ms, Memory: 113760 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -3,45 +3,63 @@ import java.io.*;
 
 public class Main {
 
-	static boolean[] v;
-	static int[][] arr;
+	static int[] path;
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 
 		st = new StringTokenizer(br.readLine());
-		int size = Integer.parseInt(st.nextToken());
+
+		int point = Integer.parseInt(st.nextToken());
 		int line = Integer.parseInt(st.nextToken());
-		arr = new int[size + 1][size + 1];
-		v = new boolean[size + 1];
+
+		Set<Integer> s = new HashSet<>();
+		path = new int[point + 1];
+
+		for (int i = 0; i < path.length; i++)
+			path[i] = i;
 
 		for (int i = 0; i < line; i++) {
 			st = new StringTokenizer(br.readLine());
-			int s = Integer.parseInt(st.nextToken());
-			int e = Integer.parseInt(st.nextToken());
 
-			arr[s][e] = 1;
-			arr[e][s] = 1;
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+
+			int roota = find(a);
+			int rootb = find(b);
+
+			if (roota != rootb) {
+				union(roota, rootb);
+			}
+
 		}
+
 		int count = 0;
-		for (int i = 1; i < size + 1; i++) {
-			if (!v[i]) {
-				v[i] = true;
-				depth(size, i);
-				++count;
+		for (int i = 1; i < path.length; i++) {
+			if (!s.contains(find(path[i]))) {
+				count++;
+				s.add(find(path[i]));
 			}
 		}
+
+		//System.out.println(Arrays.toString(path));
 		System.out.println(count);
 	}
 
-	static void depth(int size, int node) {
-		for (int i = 0; i < size + 1; i++) {
-			if (arr[node][i] == 1 && !v[i]) {
-				v[i] = true;
-				depth(size, i);
-			}
+	static int find(int n) {
+		if (path[n] == n) {
+			return n;
+		}
+		return path[n] = find(path[n]);
+	}
+
+	static void union(int a, int b) {
+		if (a < b) {
+			path[b] = a;
+		} else {
+			path[a] = b;
 		}
 	}
 }
