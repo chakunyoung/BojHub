303a451ed121e03ab86c2b9c1c3019678c67dbc9
cha kun young <chakunyou@naver.com>
2024-01-01 23:06:42 +0900

    [Bronze III] Title: 완전 제곱수, Time: 124 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Bronze/6131. 완전 제곱수/완전 제곱수.java
+++ 백준/Bronze/6131. 완전 제곱수/완전 제곱수.java
@@ -1,24 +1,23 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
 
-		int number = Integer.parseInt(br.readLine());
+		int g = Integer.parseInt(br.readLine());
 		int count = 0;
-		for(int i = 1; i<=500; i++){
-			for(int j = 1; j<=500; j++){
-				int n1 = i*i;
-				int n2 = j*j;
 
-				if(n1 - n2 == number)
+		for (int i = 1; i <= 500; i++) {
+			for (int j = 1; j <= i; j++) {
+				if (i * i - j * j == g) {
 					count++;
+				}
 			}
 		}
+
 		System.out.println(count);
 	}
 }
