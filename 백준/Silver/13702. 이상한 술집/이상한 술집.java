ed2d9c5998d6b28fc5f0ae59dcc744c756e4d477
cha kun young <chakunyou@naver.com>
2024-01-27 12:46:30 +0900

    [Silver III] Title: 이상한 술집, Time: 176 ms, Memory: 17424 KB -BaekjoonHub

--- 백준/Silver/13702. 이상한 술집/이상한 술집.java
+++ 백준/Silver/13702. 이상한 술집/이상한 술집.java
@@ -1,50 +1,48 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int[] arr;
-    static int child;
-    static int p;
-    static long ans;
 
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
-        
-        st = new StringTokenizer(br.readLine());
-        
-        int n = Integer.parseInt(st.nextToken());
-        p = Integer.parseInt(st.nextToken());
-        
-        arr = new int[n];
-        for(int i = 0; i<n; i++) 
-        	arr[i] = Integer.parseInt(br.readLine());
-        
-        bs();
-        System.out.println(ans);
-        
-    }
-    
-    public static void bs() {
-    	long s = 1;
-    	long e = Integer.MAX_VALUE;
-    	long mid = 0;
-    	
-    	while(s<=e) {
-    		mid = (s + e) / 2;
-    		int count = 0;
-    		for(int ele : arr) {
-    			if(ele >= mid) 
-    				count += ele / mid;
-    		}
-    		
-    		if(count >= p) {
-    			ans = mid;
-    			s = mid + 1;
-    		}else 
-    			e = mid - 1;
-    	}
-    	
-    }
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		st = new StringTokenizer(br.readLine());
+
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = Integer.parseInt(br.readLine());
+		}
+
+		System.out.println(bs(arr, k));
+
+	}
+
+	public static int bs(int[] arr, int k) {
+		int s = 0;
+		int e = Integer.MAX_VALUE;
+		int ans = 0;
+
+		while (s <= e) {
+			long mid = ((long) s + e) / 2;
+
+			int count = 0;
+			for (int ele : arr) {
+				count += ele / mid;
+			}
+
+			if (count >= k) {
+				s = (int) mid + 1;
+				ans = (int) mid;
+			} else {
+				e = (int) mid - 1;
+			}
+
+		}
+		return ans;
+	}
 }
