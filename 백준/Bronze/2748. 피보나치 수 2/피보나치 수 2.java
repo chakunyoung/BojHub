53811eb92c3e6273bd24a21173f007ca743b1556
cha kun young <chakunyou@naver.com>
2024-02-17 16:51:02 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 124 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		int n = Integer.parseInt(br.readLine());
+
+		long[] memo = new long[n + 1];
+		memo[1] = 1;
+		for (int i = 2; i <= n; i++) {
+			memo[i] = memo[i - 1] + memo[i - 2];
+		}
+		System.out.print(memo[n]);
+	}
+}
