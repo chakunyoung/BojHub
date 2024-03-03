28405ce879eeb00da4948b14f70203c7e801bc05
cha kun young <chakunyou@naver.com>
2024-03-03 09:44:31 +0900

    [Bronze I] Title: 이진수 덧셈, Time: 124 ms, Memory: 14256 KB -BaekjoonHub

--- 백준/Bronze/2729. 이진수 덧셈/이진수 덧셈.java
+++ 백준/Bronze/2729. 이진수 덧셈/이진수 덧셈.java
@@ -17,32 +17,10 @@ public class Main {
 			String s1 = st.nextToken();
 			String s2 = st.nextToken();
 
-			BigInteger bi = new BigInteger(s1, 2);
-			s1 = bi.toString(2);
-
-			bi = new BigInteger(s2, 2);
-			s2 = bi.toString(2);
-
-			if (s1.length() < s2.length()) {
-				String t = "";
-				t = s2;
-				s2 = s1;
-				s1 = t;
-			}
-
-			s2 = "0".repeat(s1.length() - s2.length()) + s2;
-			StringBuilder res = new StringBuilder();
-			int carry = 0;
-			for (int j = s1.length() - 1; j >= 0; j--) {
-				int sum = (s1.charAt(j) - '0') + (s2.charAt(j) - '0') + carry;
-				res.insert(0, sum % 2);
-				carry = sum / 2;
-			}
-
-			if (carry != 0)
-				res.insert(0, carry);
-
-			sb.append(res.toString()).append("\n");
+			BigInteger b1 = new BigInteger(s1, 2);
+			BigInteger b2 = new BigInteger(s2, 2);
+			BigInteger sum = b1.add(b2);
+			sb.append(sum.toString(2)).append("\n");
 		}
 		System.out.println(sb.toString());
 	}
