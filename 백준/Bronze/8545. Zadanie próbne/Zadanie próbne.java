612d0cf13556b4bcac833c5c0037c73293cdb7e4
cha kun young <chakunyou@naver.com>
2024-03-23 12:18:46 +0900

    [Bronze V] Title: Zadanie próbne, Time: 124 ms, Memory: 14088 KB -BaekjoonHub

---
+++ 백준/Bronze/8545. Zadanie próbne/Zadanie próbne.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+
+		sb = new StringBuilder(br.readLine());
+		System.out.println(sb.reverse());
+
+	}
+}
