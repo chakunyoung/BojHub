6dd1fe77c5be2bd1320ca8d5c77cd1eebacf9622
cha kun young <chakunyou@naver.com>
2024-09-23 22:29:23 +0900

    [Bronze I] Title: 슈퍼 마리오, Time: 96 ms, Memory: 14160 KB -BaekjoonHub

--- 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.java
@@ -2,23 +2,36 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] arr = new int[10];
 
-        for (int i = 0; i < 10; i++)
-            arr[i] = Integer.parseInt(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
 
-        int min = Integer.MAX_VALUE;
-        int answer = 0;
-        int sum = 0;
-        for (int i = 0; i < 10; i++) {
-            sum += arr[i];
-            if (min >= Math.abs(sum - 100)) {
-                min = Math.abs(sum - 100);
-                answer = sum;
-            }
-        }
-        System.out.println(answer);
-    }
+		List<Integer> list = new ArrayList<>();
+
+		for (int i = 0; i < 10; i++)
+			list.add(Integer.parseInt(br.readLine()));
+
+		int sum = 0;
+
+		for (int i = 0; i < list.size(); i++) {
+			int localSum = sum + list.get(i);
+			if (localSum > 100) {
+				int temp = localSum - 100;
+				int temp2 = 100 - sum;
+
+				if (temp > temp2) {
+					System.out.println(sum);
+					return;
+				} else {
+					System.out.println(localSum);
+					return;
+				}
+			} else {
+				sum += list.get(i);
+			}
+		}
+		System.out.println(sum);
+	}
 }
