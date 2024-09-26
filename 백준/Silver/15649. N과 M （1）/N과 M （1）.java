51af35c5104e54772bfa627a415f7e1f871e1bbf
cha kun young <chakunyou@naver.com>
2024-09-26 22:32:19 +0900

    [Silver III] Title: N과 M (1), Time: 236 ms, Memory: 22860 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,54 +1,46 @@
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.io.*;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
-// 문제 풀이 용도
 public class Main {
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringBuilder sb = new StringBuilder();
 
-    static int[] arr;
-    static int[] tmp;
-    static boolean[] v;
+	static StringBuilder sb = new StringBuilder();
+	static int n;
+	static int m;
+	static boolean[] v;
+	static int[] arr;
 
-    public static void main(String[] args) throws IOException {
-        StringTokenizer st = new StringTokenizer(br.readLine());
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-        int numberLimit = Integer.parseInt(st.nextToken());
-        int jari = Integer.parseInt(st.nextToken());
-        v = new boolean[numberLimit];
-        arr = new int[numberLimit];
-        for(int i = 0; i<numberLimit; i++)
-            arr[i] = i+1;
+		st = new StringTokenizer(br.readLine());
 
-        tmp = new int[jari];
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		arr = new int[m];
+		v = new boolean[n + 1];
 
-        su(jari, 0);
-        System.out.println(sb);
-    }
+		recur(0, 1);
+		System.out.println(sb.toString());
 
-    public static void su(int limit, int idx){
-        if (limit == idx){
-            for(int n : tmp){
-                sb.append(n).append(" ");
-            }
-            sb.append("\n");
-            return;
-        }
+	}
 
+	static void recur(int a, int b) {
+		if (a == m) {
+			for (int ele : arr)
+				sb.append(ele).append(" ");
+			sb.append("\n");
+			return;
+		}
 
-        for(int i = 0; i<arr.length; i++){
-            if(!v[i]) {
-                v[i] = true;
-                tmp[idx] = arr[i];
-                su(limit, idx + 1);
-                v[i] = false;
-            }
-        }
-    }
+		for (int i = 1; i <= n; i++) {
+			if (!v[i]) {
+				v[i] = true;
+				arr[a] = i;
+				recur(a + 1, i + 1);
+				v[i] = false;
+			}
+		}
+
+	}
 }
