fce8722899e64024b2e5088f8cc661ba87dd417e
cha kun young <chakunyou@naver.com>
2024-09-28 19:59:30 +0900

    [Gold V] Title: 최소비용 구하기, Time: 496 ms, Memory: 52388 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -2,65 +2,68 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	
-	static Queue<Node> q = new PriorityQueue<>((o1,o2) -> o1.dist - o2.dist);
+
+	static Queue<Node> q = new PriorityQueue<>((o1, o2) -> o1.w - o2.w);
 	static List<List<Node>> list;
 	static int[] dists;
-	
+
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st;
-		
+
 		int nodes = Integer.parseInt(br.readLine());
 		int edges = Integer.parseInt(br.readLine());
-		
+
 		list = new ArrayList<>();
-		for(int i = 0; i<=nodes; i++) list.add(new ArrayList<>());
-		
-		for(int i = 0; i<edges; i++) {
+		for (int i = 0; i <= nodes; i++)
+			list.add(new ArrayList<>());
+
+		for (int i = 0; i < edges; i++) {
 			st = new StringTokenizer(br.readLine());
 			int s = Integer.parseInt(st.nextToken());
 			int e = Integer.parseInt(st.nextToken());
 			int dist = Integer.parseInt(st.nextToken());
-			
+
 			list.get(s).add(new Node(e, dist));
 		}
-		
+
 		st = new StringTokenizer(br.readLine());
 		int s = Integer.parseInt(st.nextToken());
 		int e = Integer.parseInt(st.nextToken());
-		
+
 		dists = new int[nodes + 1];
-		for(int i = 1; i<dists.length; i++) 
-			dists[i] = Integer.MAX_VALUE>>1 ;
+		Arrays.fill(dists, Integer.MAX_VALUE >> 1);
 		dists[s] = 0;
 		q.offer(new Node(s, 0));
 		dijk();
 		System.out.println(dists[e]);
 	}
+
 	public static void dijk() {
-		while(!q.isEmpty()) {
+		while (!q.isEmpty()) {
 			Node mid = q.poll();
-			
-			if(mid.dist > dists[mid.y])
+
+			if (mid.w > dists[mid.y])
 				continue;
-			
-			for(Node dest : list.get(mid.y)) {
-				if(dists[mid.y] + dest.dist < dists[dest.y]) {
-					dists[dest.y] = dists[mid.y] + dest.dist;
-					q.offer(new Node(dest.y, dists[mid.y] + dest.dist));
+
+			// dest 가 처리되어 있는지 확인
+			// 현재 dist[dest] 보다 (dist[mid] + 해당 가중치)가 작다면 현재 값 으로 update
+			for (Node dest : list.get(mid.y)) {
+				if (dists[mid.y] + dest.w < dists[dest.y]) {
+					dists[dest.y] = dists[mid.y] + dest.w;
+					q.offer(new Node(dest.y, dists[mid.y] + dest.w));
 				}
 			}
 		}
 	}
+}
 
-	static class Node {
-		int y, dist;
+class Node {
+	int y, w;
 
-		public Node(int y, int dist) {
-			this.y = y;
-			this.dist = dist;
-		}
+	public Node(int y, int w) {
+		this.y = y;
+		this.w = w;
 	}
 }
