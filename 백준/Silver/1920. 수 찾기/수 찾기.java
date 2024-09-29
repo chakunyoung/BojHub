ead84e0834cbc19aef1a0c6964023e29ca2cb8fa
cha kun young <chakunyou@naver.com>
2024-09-29 22:13:06 +0900

    [Silver IV] Title: 수 찾기, Time: 624 ms, Memory: 43932 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -5,40 +5,41 @@ public class Main {
 
 	public static void main(String[] args) throws Exception {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-		int size = Integer.parseInt(br.readLine());
-		int[] arr = new int[size];
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int idx = 0;
-		while (st.hasMoreTokens())
-			arr[idx++] = Integer.parseInt(st.nextToken());
-
-		Arrays.sort(arr);
-		
 		StringBuilder sb = new StringBuilder();
-		br.readLine();
+		StringTokenizer st;
+
+		int n = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
+
+		int[] arr = new int[n];
+		int idx = 0;
+		while (st.hasMoreTokens()) {
+			arr[idx++] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		n = Integer.parseInt(br.readLine());
 		st = new StringTokenizer(br.readLine());
 		while (st.hasMoreTokens()) {
-			int n = Integer.parseInt(st.nextToken());
-			n = bs(arr, n);
-			sb.append(n).append("\n");
+			int f = Integer.parseInt(st.nextToken());
+			sb.append(bs(f, arr)).append("\n");
 		}
 		System.out.println(sb.toString());
-
 	}
 
-	public static int bs(int[] arr, int find){
+	static int bs(int target, int[] arr) {
 		int s = 0;
 		int e = arr.length - 1;
-		
-		while(s <= e) {
-			int mid = (s+e)/2;
-			if(arr[mid] == find)
-				return 1; 
-			else if(find > arr[mid]) {
+		int t = arr.length;
+
+		while (s <= e) {
+			int mid = (s + e) / 2;
+
+			if (target > arr[mid]) {
 				s = mid + 1;
-			}else
+			} else if (target < arr[mid]) {
 				e = mid - 1;
+			} else
+				return 1;
 		}
 		return 0;
 	}
