b6bfbc9b0dbcfb70d38ed29021441295e74188ad
cha kun young <chakunyou@naver.com>
2024-10-01 23:13:49 +0900

    [Gold III] Title: 게임 개발, Time: 216 ms, Memory: 21548 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.java
+++ 백준/Gold/1516. 게임 개발/게임 개발.java
@@ -2,65 +2,66 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-
 	static List<List<Integer>> list = new ArrayList<>();
-	static Queue<Integer> q = new LinkedList<Integer>();
-
 	static int[] indegree;
-	static int[] times;
-	static int[] totalTimes;
+	static int[] time;
+	static int[] ans;
+	static StringBuilder sb = new StringBuilder();
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 
-		int size = Integer.parseInt(br.readLine());
+		int n = Integer.parseInt(br.readLine());
 
-		times = new int[size + 1];
-		totalTimes = new int[size + 1];
-		indegree = new int[size + 1];
+		indegree = new int[n + 1];
+		time = new int[n + 1];
+		ans = new int[n + 1];
+		list = new ArrayList<>();
 
-		for (int i = 0; i < size + 1; i++)
+		for (int i = 0; i <= n; i++) {
 			list.add(new ArrayList<>());
+		}
 
-		for (int i = 1; i < size + 1; i++) {
+		for (int i = 1; i <= n; i++) {
 			st = new StringTokenizer(br.readLine());
+			time[i] = Integer.parseInt(st.nextToken());
+			ans[i] = time[i];
 
-			if (st.countTokens() >= 3) {
-				int v = Integer.parseInt(st.nextToken());
-				times[i] = v;
-				
-				int nidx = 0;
-				while ((nidx = Integer.parseInt(st.nextToken())) != -1) {
-					list.get(nidx).add(i);
-					indegree[i]++;
-				}
-			} else {
-				times[i] = Integer.parseInt(st.nextToken());
-				q.offer(i);
+			while (true) {
+				int build = Integer.parseInt(st.nextToken());
+				if (build == -1)
+					break;
+
+				list.get(build).add(i);
+				indegree[i]++;
 			}
 		}
-		
-		ts();
 
-		for (int i = 0; i < times.length; i++)
-			totalTimes[i] += times[i];
+		ts(n);
 
-		StringBuilder sb = new StringBuilder();
-		for (int i = 1; i < totalTimes.length; i++)
-			sb.append(totalTimes[i]).append("\n");
+		for (int i = 1; i <= n; i++)
+			sb.append(ans[i]).append("\n");
 
 		System.out.println(sb.toString());
 	}
 
-	public static void ts() {
-		while (!q.isEmpty()) {
-			int n = q.poll();
+	static void ts(int n) {
+		Queue<Integer> q = new LinkedList<>();
 
-			for (int next : list.get(n)) {
+		for (int i = 1; i <= n; i++) {
+			if (indegree[i] == 0) {
+				q.offer(i);
+			}
+		}
+
+		while (!q.isEmpty()) {
+			int c = q.poll();
+
+			for (int next : list.get(c)) {
 				indegree[next]--;
-				
-				totalTimes[next] = Math.max(totalTimes[next], totalTimes[n] + times[n]);
+				ans[next] = Math.max(ans[next], ans[c] + time[next]); // 다 지어져야 함
+
 				if (indegree[next] == 0) {
 					q.offer(next);
 				}
